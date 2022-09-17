22962381f36dfe84075f6af7cf28da78bd3fde28
zhangheng <490724276@qq.com>
2022-09-17 14:56:00 +0800

    在dev分支提交了代码

--- test.js
+++ test.js
@@ -7,3 +7,4 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 console.log('测试代码')
+console.log("测试一下合并分支");
