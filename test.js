a593eb1f20c4dd4255618ae5834671ec42944993
zhangheng <490724276@qq.com>
2022-09-17 13:48:56 +0800

    新增css/js

---
+++ test.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: zhangheng 490724276@qq.com
+ * @Date: 2022-09-17 13:44:43
+ * @LastEditors: zhangheng 490724276@qq.com
+ * @LastEditTime: 2022-09-17 13:44:52
+ * @FilePath: \git-test\test.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+console.log('测试代码')
